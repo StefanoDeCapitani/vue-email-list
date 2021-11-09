4840ef91e0c57bf2d9ae80423ad158693df958c9
Stefano De Capitani <stefano.deca@live.com>
2021-11-09 16:18:47 +0100

    Show emails after getting all the responses

--- js/main.js
+++ js/main.js
@@ -12,19 +12,29 @@ window.addEventListener("DOMContentLoaded", function () {
     el: "#root",
     data: {
       emails: [],
+      pendingApiCalls: 0,
     },
     methods: {
       getMultipleEmails(numOfEmails) {
+        numOfEmails = parseInt(numOfEmails);
+        if (isNaN(numOfEmails) || this.pendingApiCalls > 0) {
+          return;
+        }
+        this.pendingApiCalls = numOfEmails;
         this.emails = [];
+        let emailsReceived = [];
         for (let i = 0; i < numOfEmails; i++) {
           axios
             .get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then((resp) => {
-              this.emails.push(resp.data?.response);
+              emailsReceived.push(resp.data?.response);
+              this.pendingApiCalls--;
+              if (this.pendingApiCalls === 0) {
+                this.emails = [...emailsReceived];
+              }
             });
         }
       },
     },
-    mounted() {},
   });
 });
