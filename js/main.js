b4fee9d8fee50a8448988ea9f7294efceb3fa408
Stefano De Capitani <stefano.deca@live.com>
2021-11-09 15:34:34 +0100

    Files added

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* Descrizione:
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. */
+
+Vue.config.devtools = true;
+
+window.addEventListener("DOMContentLoaded", function () {
+  const vm = new Vue({
+    el: "#root",
+    data: {
+      message: "ciao",
+    },
+    methods: {},
+    mounted() {},
+  });
+});
